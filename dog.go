75f9c4169bd57e2205876481b80f39eedf72d3b1
yukai <83336281+yukailin-git@users.noreply.github.com>
2023-08-31 14:55:51 +0800

    second commit

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string{
+	return "When: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sunnylinhome/dog
+
+go 1.21.0
